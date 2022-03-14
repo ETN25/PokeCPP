b4fcc814f9a4108348a726c45a14deb807b52e69
ETN5 <me@artfx.fr>
2022-03-14 11:20:20 +0100

    1er push

---
+++ MAGOUS_Etienne_C++Project/MAGOUS_Etienne_C++Project.cpp
@@ -0,0 +1,15 @@
+// MAGOUS_Etienne_C++Project.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World! hey\n" << endl;
+    string answer;
+    cin >> answer;
+    cout << "\n" + answer << endl;
+}
+
